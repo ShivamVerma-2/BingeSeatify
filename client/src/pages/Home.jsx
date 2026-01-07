52053b99a83476b06e582332f392057c1deb95a0
Shivam Verma <sv863117@gmail.com>
2026-01-07 23:51:27 +0530

    updated the codes for the UI

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import HeroSection from '../components/HeroSection'
 import FeatureSection from '../components/FeatureSection'
 import BlurCircle from '../components/BlurCircle'
-import TrailersSection from '../components/TrailersSection'
+// import TrailersSection from '../components/TrailersSection'
+import Trailers from '../components/trailers'
 
 const Home = () => {
   return (
     <> <HeroSection />
      <FeatureSection/>
      <BlurCircle/>
-     <TrailersSection/>
+     <Trailers/>
     </>
     
 
